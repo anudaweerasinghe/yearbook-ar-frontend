baa6711d7f8ebd05abaf97daf7fb2ca9fcfb6fea
anudaw <anuda@live.com>
2020-03-08 13:05:15 +0530

    Added instructions to home page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -6,8 +6,6 @@ import 'package:async/async.dart';
 import 'dart:typed_data';
 import 'video.dart';
 
-
-
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => new _HomeScreenState();
@@ -24,43 +22,85 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: PreferredSize(
-          child: new AppBar(
-            elevation: 0,
-            title: new Text(
-              'OSC Augmented Yearbook',
-              textAlign: TextAlign.center,
-              style: new TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 22,
+        appBar: PreferredSize(
+            child: new AppBar(
+              elevation: 0,
+              title: new Text(
+                'OSC Augmented Yearbook',
+                textAlign: TextAlign.center,
+                style: new TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22,
+                ),
               ),
-            ),
-            flexibleSpace: Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.centerLeft,
-                  end: Alignment.centerRight,
-                  colors: [
-                    new Color.fromARGB(255, 252, 70, 107),
-                    new Color.fromARGB(255, 63, 94, 251)
-                  ],
+              flexibleSpace: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                    colors: [
+                      new Color.fromARGB(255, 252, 70, 107),
+                      new Color.fromARGB(255, 63, 94, 251)
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-          preferredSize: Size.fromHeight(55)
-      ),
-      body: Center(
-        child: Column(
+            preferredSize: Size.fromHeight(55)),
+        body: Center(
+            child: Column(
           children: <Widget>[
+            Padding(
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Text("Welcome!",
+                        textAlign: TextAlign.left,
+                        style: new TextStyle(
+                            fontSize: 30.0,
+                            fontWeight: FontWeight.w900,
+                            fontFamily: 'Roboto')),
+                    Padding(padding: EdgeInsets.all(10),),
+                    Text("1. Buy a copy of the 2019-2020 Yearbook from the OSC Library",
+                        textAlign: TextAlign.left,
+                        style: new TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.w500,
+                            fontFamily: 'Roboto')),
+                    Padding(padding: EdgeInsets.all(5),),
+                    Text("2. Find a page with Augmented Content",
+                        textAlign: TextAlign.left,
+                        style: new TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.w500,
+                            fontFamily: 'Roboto')),
+                    Padding(padding: EdgeInsets.all(5),),
+                    Text("3. Scan the QR Code by pressing the button below",
+                        textAlign: TextAlign.left,
+                        style: new TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.w500,
+                            fontFamily: 'Roboto')),
+                    Padding(padding: EdgeInsets.all(5),),
+                    Text("4. Relive the memories and Enjoy!",
+                        textAlign: TextAlign.left,
+                        style: new TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.w500,
+                            fontFamily: 'Roboto')),
+                  ],
+                )),
             new Container(
-              margin: new EdgeInsets.symmetric(horizontal: 30.0, vertical: 30.0),
-              child:RaisedButton(
+              margin:
+                  new EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+              child: RaisedButton(
                   onPressed: scan,
                   textColor: Colors.white,
                   color: Colors.white,
                   padding: const EdgeInsets.all(0.0),
-                  child:Container(
+                  child: Container(
                     width: double.infinity,
                     decoration: BoxDecoration(
                       gradient: LinearGradient(
@@ -73,23 +113,14 @@ class _HomeScreenState extends State<HomeScreen> {
                       ),
                     ),
                     padding: const EdgeInsets.all(10.0),
-                    child: const Text(
-                        'Scan for Augmented Content',
+                    child: const Text('Scan for Augmented Content',
                         textAlign: TextAlign.center,
-                        style: TextStyle(fontSize: 26, fontWeight: FontWeight.w600)
-                    ),
-                  )
-              ),
+                        style: TextStyle(
+                            fontSize: 26, fontWeight: FontWeight.w600)),
+                  )),
             ),
-            Text(barcode, textAlign: TextAlign.center,)
           ],
-        )
-
-      )
-
-
-    );
-
+        )));
   }
 
   Future scan() async {
@@ -99,11 +130,10 @@ class _HomeScreenState extends State<HomeScreen> {
       Navigator.push(
         context,
         new MaterialPageRoute(
-            builder: (ctxt) =>
-            new VideoScreen(
-              qrText: barcode,)),
+            builder: (ctxt) => new VideoScreen(
+                  qrText: barcode,
+                )),
       );
-
     } on PlatformException catch (e) {
       if (e.code == BarcodeScanner.CameraAccessDenied) {
         setState(() {
@@ -112,8 +142,9 @@ class _HomeScreenState extends State<HomeScreen> {
       } else {
         setState(() => this.barcode = 'Unknown error: $e');
       }
-    } on FormatException{
-      setState(() => this.barcode = 'null (User returned using the "back"-button before scanning anything. Result)');
+    } on FormatException {
+      setState(() => this.barcode =
+          'null (User returned using the "back"-button before scanning anything. Result)');
     } catch (e) {
       setState(() => this.barcode = 'Unknown error: $e');
     }
